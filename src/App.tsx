fb1aa4b4f07f9b32c12fe08bd77bc5a426b38a65
Cass Chin <casschin@gmail.com>
2020-02-15 17:42:49 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,67 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import "./App.css";
 import WaveSurfer from "wavesurfer.js";
 
 function App() {
+  const filenames = ["satc", "boom"];
   return (
     <div className="App">
-      <Waveform />
-      <div id="waveform" />
+      {filenames.map(filename => (
+        <Waveform key={`waveform-${filename}`} filename={filename} />
+      ))}
     </div>
   );
 }
 
-function Waveform() {
+function Waveform({ filename }: { filename: string }) {
+  const [isCanvasLoaded, setIsCanvasLoaded] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [isReady, setIsReady] = useState(false);
+  const [isWaveformReady, setIsWaveformReady] = useState(false);
   const wavesurferRef = useRef<WaveSurfer | null>(null);
+  const canvasRef = useCallback(node => {
+    if (node !== null) {
+      setIsCanvasLoaded(true);
+    }
+  }, []);
+  const id = `canvas-${filename}`;
   useEffect(() => {
     const wavesurferInstance = WaveSurfer.create({
-      container: "#waveform",
+      container: `#${id}`,
       waveColor: "violet",
       progressColor: "purple"
     });
-    wavesurferInstance.load("/audio/satc.mp3");
+    wavesurferInstance.load(`/audio/${filename}.mp3`);
     wavesurferInstance.on("ready", function() {
-      setIsReady(true);
+      setIsWaveformReady(true);
     });
     wavesurferRef.current = wavesurferInstance;
-  }, []);
+  }, [filename]);
   const waveform = wavesurferRef.current;
-  if (waveform === null || isReady === false) return null;
+  // if (waveform === null || isWaveformReady === false) return null;
   return (
     <div>
       <h2>{isPlaying}</h2>
-      <button
-        onClick={() => {
-          isPlaying ? waveform.pause() : waveform.play();
-          setIsPlaying(waveform.isPlaying());
-        }}
-      >
-        {isPlaying ? "pause" : "play"}
-      </button>
-      <button
-        onClick={() => {
-          waveform.stop();
-          setIsPlaying(false);
-        }}
-      >
-        stop
-      </button>
+      {waveform !== null && (
+        <>
+          <button
+            onClick={() => {
+              isPlaying ? waveform.pause() : waveform.play();
+              setIsPlaying(waveform.isPlaying());
+            }}
+          >
+            {isPlaying ? "pause" : "play"}
+          </button>
+          <button
+            onClick={() => {
+              waveform.stop();
+              setIsPlaying(false);
+            }}
+          >
+            stop
+          </button>
+        </>
+      )}
+      <div id={id} ref={canvasRef}></div>
     </div>
   );
 }
